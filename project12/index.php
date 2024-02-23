92fed532ff0e2884837600da75b2496c5a1667c0
longmoc12 <longmoc95@gmail.com>
2024-02-23 11:31:31 +0700

    update

--- project12/index.php
+++ project12/index.php
@@ -11,46 +11,51 @@
         "LIÊN HỆ"
        ];      
 ?>
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project12</title>
-    <link rel="stylesheet" href="./boxicons-2.1.4/boxicons-2.1.4/css/boxicons.min.css">
-    <link rel="stylesheet" href="./bootstrap/bootstrap-5.3.0-alpha1-dist/css/bootstrap.min.css">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
     <style>
-        .nav{
-            background-color: #0a1f77;
-            color: #fff;
-            display: flex;
-            justify-content: space-around;
+        .header{
+            background-color: #0a53be;
+        }
+        .navitem{
+            display: flex ;
+            height: 50px;
             align-items: center;
+            justify-content: space-around;
         }
-        .text{
-            color: #fff;
+        li{
+            list-style: none;
+
+        }
+        span{
+            font-size: 20px;
+            color: white;
             padding-left: 20px;
-            font-family: 'Times New Roman', Times, serif;
-        }
-        .line{
-            border-left: 2px solid #fff;
-        }
-        .line:hover {
-            color: yellow;
-            cursor: pointer;
-        }
-        .bxs-home{
-            color: #fff;
-            font-size: 30px;
+            /*background-color: red;*/
+            border-left: 3px solid white;
         }
     </style>
+
 </head>
 <body>
-    <div class="nav container-fluid">
-        <div class="container-nav"><a href=""><i class='bx bxs-home chu'></i></a></div>
-        <?php foreach ($navItems as $item) {
-            echo '<div class="container-nav"><a href=""><label for="" class="text line">' . $item . '</label></a></div>';
-        } ?>
+    <div class = header>
+        <ul class = "navitem">
+            <?php
+            foreach ($navItems as $item){
+                echo "<li> <span>$item</span> </li>";
+            }
+            ?>
+        </ul>
     </div>
 </body>
 </html>
+
+
--- project13/index.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "oi doi oi";
